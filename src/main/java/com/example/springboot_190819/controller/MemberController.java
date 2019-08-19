a8cdb3f93c48bedbffe1b1285b41270801dd7cd4
inchic <poiuy1709@naver.com>
2019-08-19 18:04:36 +0900

    inchic

---
+++ src/main/java/com/example/springboot_190819/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.example.springboot_190819.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController{
+    @RequestMapping(value="/member/list")
+    public void memberAction(){
+        //retrun이 없다면 value="/member/list.html을 찾는다"
+    }
+}
